68d40a2c5e1b1a7730290e1572a2bf504c9a8291
Joshua321V <joshuareburiano21@gmail.com>
2025-09-02 15:04:51 +0800

    Java Activities 2nd year collage

---
+++ OOPIntro.java
@@ -0,0 +1,11 @@
+public class OOPIntro {
+
+    public static void main(String[] args) {
+        
+        student info = new student();
+        info.setDetails("Joshua V. Reburiano", 21, "BSCS");
+
+        info.dispalyInfo();
+    }
+    
+}
